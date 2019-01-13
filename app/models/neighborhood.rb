773de5fc2feeaa1d2c9699935dbf2b9c5b9adfa7
Fer Hernandez <fh.1111@gmail.com>
2019-01-13 20:03:04 +0000

    Automatically backed up by Learn

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,12 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
 
+  has_many :reservations, through: :listings
+
+  incluse Reservable
+
+  def neighborhood_openings(start_date, end_date)
+    openings(start_date, end_date)
+  end
+
 end
