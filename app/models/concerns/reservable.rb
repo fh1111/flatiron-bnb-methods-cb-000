4b1cdb83ab19293a4a45181010b2eaa9263f5752
Fer Hernandez <fh.1111@gmail.com>
2019-01-13 21:05:22 +0000

    Automatically backed up by Learn

--- app/models/concerns/reservable.rb
+++ app/models/concerns/reservable.rb
@@ -16,13 +16,13 @@ class_methods do
     # does not currently cover, so would need to be added.
     def highest_ratio_reservations_to_listings
 
-      all.max do |a, b|
+      self.max do |a, b|
         a.ratio_reservations_to_listings <=> b.ratio_reservations_to_listings
       end
     end
 
     def most_reservations
-      all.max do |a, b|
+      self.max do |a, b|
         a.reservations.count <=> b.reservations.count
       end
     end
